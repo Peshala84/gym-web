f5e5c9edea30f4a115975853fcfea113884b582d
Ramesh Peshala <rameshpeshala84@gmail.com>
2025-02-12 00:37:23 +0530

    Add promotional text and styling to Hero component for enhanced user engagement

--- src/components/Hero/Hero.jsx
+++ src/components/Hero/Hero.jsx
@@ -1,19 +1,33 @@
 import React from 'react'
 import './Hero.css'
+import '../../App.css'
 import Header from '../Header/Header'
 
 const Hero = () => {
-  return (
-    <div className="hero">
-       <div className="left-h">
-        <Header/>
-         
-       </div>
-       <div className="right-h">
-            right side
-       </div>
-    </div>
-  )
+    return (
+        <div className="hero">
+            <div className="left-h">
+                <Header />
+                <div className="the-best-ad">
+                    <div></div>
+                    <span>The best fitness club in the Sri Lanka</span>
+                </div>
+
+                <div className="hero-text">
+                    <div>
+                        <span className='stroke-text'>Shape  </span>
+                        <span>Your </span>
+                    </div>
+                    <div>
+                        <span>Ideal body</span>
+                    </div>
+                </div>
+            </div>
+            <div className="right-h">
+                right side
+            </div>
+        </div>
+    )
 }
 
 export default Hero
