0305467bdba7bb60382f3919efd7aa3556876e08
Ramesh Peshala <rameshpeshala84@gmail.com>
2025-02-12 00:37:05 +0530

    Add header menu items for improved navigation

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -4,7 +4,16 @@ import logo from '../../assets/logo.png'
 const header = () => {
   return (
     <div className="header">
-      <img src={logo} alt=""  className='logo'/>
+      <img src={logo} alt="" className='logo' />
+
+      <ul className='header-menu'>
+        <li>Home</li>
+        <li>Programs</li>
+        <li>Why us</li>
+        <li>Plans</li>
+        <li>Testimonials</li>
+      </ul>
+
     </div>
   )
 }
