dbf461d569ff4eb383632a3e78cfefed17cf305a
Ramesh Peshala <rameshpeshala84@gmail.com>
2025-02-15 20:53:51 +0530

    Enhance Hero component with additional content and visuals for improved user engagement

--- src/components/Hero/Hero.jsx
+++ src/components/Hero/Hero.jsx
@@ -2,6 +2,10 @@ import React from 'react'
 import './Hero.css'
 import '../../App.css'
 import Header from '../Header/Header'
+import hero_image from "../../assets/hero_image.png"
+import hero_image_back from "../../assets/hero_image_back.png"
+import Heart from "../../assets/heart.png"
+import Calories from "../../assets/calories.png"
 
 const Hero = () => {
     return (
@@ -13,7 +17,7 @@ const Hero = () => {
                     <span>The best fitness club in the Sri Lanka</span>
                 </div>
 
-                <div className="hero-text">
+                <div className="hero-text" >
                     <div>
                         <span className='stroke-text'>Shape  </span>
                         <span>Your </span>
@@ -21,10 +25,49 @@ const Hero = () => {
                     <div>
                         <span>Ideal body</span>
                     </div>
+                    <div>
+                        <span>
+                            In here we will help you to share and build your ideal body and live uup your life to fullest
+                        </span>
+                    </div>
+                </div>
+                <div className="figures">
+                    <div>
+                        <span>+140</span>
+                        <span>expert coachs</span>
+                    </div>
+                    <div>
+                        <span>+50</span>
+                        <span>members joined</span>
+                    </div>
+                    <div>
+                        <span>+50</span>
+                        <span>fitness programs</span>
+                    </div>
+                </div>
+                <div className="hero-buttons">
+                    <button className="btn">Get Started</button>
+                    <button className="btn">Learn More</button>
                 </div>
             </div>
             <div className="right-h">
-                right side
+                <button className='btn'>Join now</button>
+
+                <div className="heart-rate">
+                    <img src={Heart} alt="" />
+                    <span>Heart Rate</span><span>116 bpm</span>
+                </div>
+
+                <img src={hero_image} alt="" className='hero-image' />
+                <img src={hero_image_back} alt="" className='hero-image-back' />
+
+                <div className="calories">
+                    <img src={Calories} alt="" />
+                    <div>
+                        <span>Calories Burned</span>
+                        <span>220 kcal</span>
+                    </div>
+                </div>
             </div>
         </div>
     )
